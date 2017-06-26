a9a3026063c475465e9bfb1dbc2d48fbd35a53d0
Misael Bejarano <vonleny@gmail.com>
2017-06-26 11:10:16 -0400

    Adding images and divs for main

--- js/mvb.js
+++ js/mvb.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-    
-    
-
+    // Side Nav for mobile
+    $(".button-collapse").sideNav({
+    	closeOnClick: true
+    });
+    $('ul.tabs').tabs({
+    	swipeable: true
+    });
   });
        
